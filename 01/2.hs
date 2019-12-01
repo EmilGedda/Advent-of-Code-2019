0a86aa12a9f79299a215c976c8a6c86bbe43ef39
Emil Gedda <emil.gedda@emilgedda.se>
2019-12-01 14:57:44 +0100

    fix

--- 01/2.hs
+++ 01/2.hs
@@ -2,7 +2,7 @@
 
 main = print . sum . map (fuel . read) . lines =<< getContents
 
-fuel n = bool 0 (m + (fuel m)) (n < i)
+fuel n
     | n < 9 = 0
     | otherwise = m + (fuel m)
     where m = n `div` 3 - 2 
