5c9c9e63440ef2bb2d880d4d8ddfd4cafe5a92b9
Emil Gedda <emil.gedda@emilgedda.se>
2019-12-02 06:54:35 +0100

    io cleanup

--- 02/1.cpp
+++ 02/1.cpp
@@ -4,27 +4,23 @@
 #include <functional>
 
 int main() {
-  char comma;
-  int x;
   std::vector<int> program;
-  while(std::cin >> x) {
+  for(int x; std::cin >> x; std::cin.get())
     program.push_back(x);
-    std::cin >> comma;
-  }
 
   program[1] = 12;
   program[2] = 2;
 
   std::array<std::function<int(int,int)>, 2> instructions = {
-    std::plus<int>(),
-    std::multiplies<int>()
+    std::plus<>(),
+    std::multiplies<>()
   };
 
   for(int i = 0; program[i] != 99; i += 4) {
     auto opcode = program[i] - 1;
-    auto a = program[i + 1];
-    auto b = program[i + 2];
-    auto dest = program[i + 3];
+    auto a      = program[i + 1];
+    auto b      = program[i + 2];
+    auto dest   = program[i + 3];
     program[dest] = instructions[opcode](program[a], program[b]);
   }
 
--- 02/2.cpp
+++ 02/2.cpp
@@ -1,43 +1,36 @@
-
 #include <vector>
 #include <iostream>
 #include <array>
 #include <functional>
 
+const std::array<std::function<int(int,int)>, 2> instructions = {
+  std::plus<>(),
+  std::multiplies<>()
+};
+
 int run(std::vector<int> program, int noun, int verb) {
   program[1] = noun;
   program[2] = verb;
 
-  std::array<std::function<int(int,int)>, 2> instructions = {
-    std::plus<int>(),
-    std::multiplies<int>()
-  };
-
   for(int i = 0; program[i] != 99; i += 4) {
     auto opcode = program[i] - 1;
-    auto a = program[i + 1];
-    auto b = program[i + 2];
-    auto dest = program[i + 3];
+    auto a      = program[i + 1];
+    auto b      = program[i + 2];
+    auto dest   = program[i + 3];
     program[dest] = instructions[opcode](program[a], program[b]);
   }
+
   return program[0];
 }
 
 int main() {
-  char comma;
-  int x;
   std::vector<int> program;
-  while(std::cin >> x) {
+  for(int x; std::cin >> x; std::cin.get()) {
     program.push_back(x);
-    std::cin >> comma;
   }
 
-  for(int i = 0; i < 99; i++) {
-    for(int j = 0; j < 99; j++) {
-      auto ret = run(program, i, j);
-      if (ret == 19690720)
+  for(int i = 0; i < 99; i++)
+    for(int j = 0; j < 99; j++)
+      if (run(program, i, j) == 19690720)
         std::cout << 100 * i + j << '\n';
-    }
-  }
-
 }
