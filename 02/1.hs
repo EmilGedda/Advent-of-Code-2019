8f8d49895d6e597fd26ae3d41915536d18755317
Emil Gedda <emil.gedda@emilgedda.se>
2019-12-02 23:41:08 +0100

    cleanup imports

--- 02/1.hs
+++ 02/1.hs
@@ -1,14 +1,13 @@
 #!/usr/bin/env runhaskell
-import Prelude hiding (head, map)
 import Data.List.Split
-import Data.Vector
+import Data.Vector as V
 
-main = print . run 0 . program . map read . fromList . splitOn "," =<< getContents
+main = print . run 0 . program . V.map read . fromList . splitOn "," =<< getContents
 
 program = (// [(1, 12), (2, 2)])
 
 run i v
-    | v ! i == 99 = head v
+    | v ! i == 99 = V.head v
     | otherwise = let instruction = toList $ slice i 4 v
                   in run (i + 4) $ (//) <*> (execute instruction) $ v
     where execute (opcode:a:b:dest:[]) v = [(dest, op opcode (v ! a) (v ! b))]
--- 02/2.hs
+++ 02/2.hs
@@ -1,9 +1,8 @@
 #!/usr/bin/env runhaskell
-import Prelude hiding (head, map, dropWhile)
 import Data.List.Split
-import Data.Vector
+import Data.Vector as V
 
-main = print . solve 0 0 . map read . fromList . splitOn "," =<< getContents
+main = print . solve 0 0 . V.map read . fromList . splitOn "," =<< getContents
 
 solve 100 j v = solve 0 (j + 1) v
 solve i j v
@@ -11,7 +10,7 @@ solve i j v
     | otherwise = solve (i + 1) j v
 
 run i v
-    | v ! i == 99 = head v
+    | v ! i == 99 = V.head v
     | otherwise = let instruction = toList $ slice i 4 v
                   in run (i + 4) $ (//) <*> (execute instruction) $ v
     where execute (opcode:a:b:dest:[]) v = [(dest, op opcode (v ! a) (v ! b))]
