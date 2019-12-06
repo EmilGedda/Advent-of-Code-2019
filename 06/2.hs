256a5b0d4543d4e4f3432e3bb9a5ee32f80bc9ac
Emil Gedda <emil.gedda@emilgedda.se>
2019-12-06 15:16:28 +0100

    math

--- 06/2.hs
+++ 06/2.hs
@@ -1,13 +1,14 @@
 #!/usr/bin/env runhaskell
 import qualified Data.Map.Strict as M
+import Data.List
 import Data.Tuple
 import Control.Arrow
 
 main = print . diff . parse =<< getContents
 
-diff = (diff' . backtrack "YOU") <*> backtrack "SAN"
-    where diff' (x:xs) (y:ys) | x == y = diff' xs ys
-                              | otherwise = length xs + length ys
+diff m = let you = backtrack "YOU" m
+             san = backtrack "SAN" m
+         in length you + length san - 2 * length (intersect you san)
 
 backtrack s = reverse . backtrack' s
     where backtrack' s m = s:(maybe [] . flip backtrack') m (M.lookup s m)
