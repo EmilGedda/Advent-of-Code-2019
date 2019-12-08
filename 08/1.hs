b59f07e75448b874cbe964b8b64602b51f7f0123
Emil Gedda <emil.gedda@emilgedda.se>
2019-12-08 10:20:02 +0100

    day 8 part one

---
+++ 08/1.hs
@@ -0,0 +1,11 @@
+#!/usr/bin/env runhaskell
+import Data.List
+import Data.Ord
+import Data.List.Split
+import Data.Char
+
+main = print . solve . chunksOf (25 * 6) . map digitToInt . init =<< getContents
+
+solve img = count 1 layer * count 2 layer
+    where layer = minimumBy (comparing (count 0)) img
+          count n = length . filter (n==)
