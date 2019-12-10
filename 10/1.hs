0ae39d48f258638c2c7be5cf74e7297bc0967e20
Emil Gedda <emil.gedda@emilgedda.se>
2019-12-10 23:29:53 +0100

    undo part 2 modifications

--- 10/1.hs
+++ 10/1.hs
@@ -1,18 +1,13 @@
 #!/usr/bin/env runhaskell
-import Control.Arrow
-import Data.Ord
-import Data.List hiding (insert)
 import Data.Set hiding (map, foldr, filter)
 import Data.Tuple
 
-main = print . solve . parse . filter (/='\n') =<< getContents
-
-solve xs = maximumBy (comparing fst) $ map (flip visible xs &&& id) xs
+main = print . maximum . (map =<< flip visible) . parse . filter (/='\n') =<< getContents
 
 parse = map (coords . fst) . filter snd . zip [0..] . map (=='#')
     where coords = swap . (`divMod` 33)
 
 visible origin = size . foldr (insert . angle origin) empty
     where angle o = uncurry atan2 . frac . swap . sub o
+          frac (x,y) = (fromIntegral x, fromIntegral y)
           sub (x,y) (x',y') = (x' - x, y' - y)
-          frac = fromIntegral *** fromIntegral
