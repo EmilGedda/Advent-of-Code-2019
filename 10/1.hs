ef8349d9fa2acd46c91dff9ca49c1759baef9187
Emil Gedda <emil.gedda@emilgedda.se>
2019-12-10 23:31:44 +0100

    shorten

--- 10/1.hs
+++ 10/1.hs
@@ -1,13 +1,10 @@
 #!/usr/bin/env runhaskell
 import Data.Set hiding (map, foldr, filter)
-import Data.Tuple
 
 main = print . maximum . (map =<< flip visible) . parse . filter (/='\n') =<< getContents
-
-parse = map (coords . fst) . filter snd . zip [0..] . map (=='#')
-    where coords = swap . (`divMod` 33)
+    where parse = map ((`divMod` 33) . fst) . filter snd . zip [0..] . map (=='#')
 
 visible origin = size . foldr (insert . angle origin) empty
-    where angle o = uncurry atan2 . frac . swap . sub o
+    where angle o = uncurry atan2 . frac . sub o
           frac (x,y) = (fromIntegral x, fromIntegral y)
           sub (x,y) (x',y') = (x' - x, y' - y)
