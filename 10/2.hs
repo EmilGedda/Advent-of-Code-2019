f9d3e4925ea4dd25c29a1d2855b460c78503ca1b
Emil Gedda <emil.gedda@emilgedda.se>
2019-12-10 20:30:58 +0100

    day 10 part two

--- 10/2.hs
+++ 10/2.hs
@@ -1,18 +1,18 @@
 #!/usr/bin/env runhaskell
 import Control.Arrow
-import Data.Ord
-import Data.List hiding (insert)
-import Data.Set hiding (map, foldr, filter)
+import Data.Map.Strict hiding (map, foldr, filter, drop, take)
+import Data.List
 import Data.Tuple
 
 main = print . solve . parse . filter (/='\n') =<< getContents
 
-solve xs = maximumBy (comparing fst) $ map (flip visible xs &&& id) xs
+solve = uncurry ((+) . (100 *)) . (!! 199) 
+        . concat . transpose . elems . visible
 
 parse = map (coords . fst) . filter snd . zip [0..] . map (=='#')
     where coords = swap . (`divMod` 33)
 
-visible origin = size . foldr (insert . angle origin) empty
-    where angle o = uncurry atan2 . frac . swap . sub o
+visible = foldr (uncurry (insertWith (++)) . (angle &&& pure)) empty
+    where angle = (-) pi . uncurry atan2 . frac . sub (23,29)
           sub (x,y) (x',y') = (x' - x, y' - y)
           frac = fromIntegral *** fromIntegral
