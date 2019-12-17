29004c19c4f71481d57899f4f9de0ffc1dbd89f2
Emil Gedda <emil.gedda@emilgedda.se>
2019-12-17 16:17:51 +0100

    simplify day 13

--- src/13/2.hs
+++ src/13/2.hs
@@ -2,11 +2,9 @@ import Data.Intcode
 import Data.Maybe
 import qualified Data.Map as M
 
-main = execStdinWith (save 0 2) $ run M.empty
+main = execStdinWith (save 0 2) $ flip run M.empty
 
-run grid effect =
-    let find t = fst . fst .  head . filter ((==) t . snd) $ M.toList grid
-    in case effect of
-         Input joystick -> run grid . joystick . signum $ find 4 - find 3
-         End            -> fromMaybe 0 $ M.lookup (-1,0) grid
-         Output x (Output y (Output tile fx)) -> flip run fx $ M.insert (x,y) tile grid
+run End g = fromMaybe 0 $ M.lookup (-1,0) g
+run (Output x (Output y (Output tile fx))) g = run fx $ M.insert (x,y) tile g
+run (Input joystick) g = run (joystick . signum $ xOf 4 - xOf 3) g
+    where xOf t = fst . fst .  head . filter ((==) t . snd) $ M.toList g
