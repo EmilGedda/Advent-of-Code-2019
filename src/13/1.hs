448894ec14435f1971a726919f5d0ad41f037b44
Emil Gedda <emil.gedda@emilgedda.se>
2019-12-17 17:32:32 +0100

    cabalize day 9

--- src/13/1.hs
+++ src/13/1.hs
@@ -1,6 +1,6 @@
 import Data.Intcode
 import Data.Map hiding (filter)
 
-main = execStdin $ length . filter (==2) . elems . flip run empty
+main = execStdin (flip run empty)
     where run (Output x (Output y (Output tile fx))) = run fx . insert (x,y) tile
-          run End = id
+          run End = length . filter (==2) . elems 
--- src/13/2.hs
+++ src/13/2.hs
@@ -1,8 +1,8 @@
 import Data.Intcode
 import Data.Map hiding (filter)
 
-main = execStdinWith (save 0 2) $ run empty
+main = execStdinWith (save 0 2) (run empty)
 
 run g End = last $ keys g
 run g (Input joystick) = run g . joystick . signum $ (g ! 4) - (g ! 3)
-run g (Output x (Output y (Output tile fx))) = run (insert tile x g) fx
+run g (Output x (Output _ (Output tile fx))) = run (insert tile x g) fx
