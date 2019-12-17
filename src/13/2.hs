b1023b43ace9a324f8df2dedea494b60e0a8ee68
Emil Gedda <emil.gedda@emilgedda.se>
2019-12-17 16:50:36 +0100

    shorten even more

--- src/13/2.hs
+++ src/13/2.hs
@@ -3,7 +3,6 @@ import Data.Map hiding (filter)
 
 main = execStdinWith (save 0 2) $ run empty
 
-run g End = g ! (-1,0)
-run g (Output x (Output y (Output tile fx))) = run (insert (x,y) tile g) fx
-run g (Input joystick) = run g $ joystick . signum $ xOf 4 - xOf 3
-    where xOf t = fst . fst .  head . filter ((==) t . snd) $ toList g
+run g End = last $ keys g
+run g (Input joystick) = run g . joystick . signum $ (g ! 4) - (g ! 3)
+run g (Output x (Output y (Output tile fx))) = run (insert tile x g) fx
