ab28fddb8290488272f17bcd69805f39cec93407
Emil Gedda <emil.gedda@emilgedda.se>
2019-12-27 21:01:36 +0100

    day 14 part one

--- src/14/1.hs
+++ src/14/1.hs
@@ -1,20 +1,34 @@
-#!/usr/bin/env runhaskell
-import qualified Data.Map as M
 import Control.Monad
-import Data.Maybe
 import Data.Either
 import Text.ParserCombinators.Parsec
-import Text.ParserCombinators.Parsec.Char
+import qualified Data.Map as M
 
-data Resource = Resource Int String deriving (Show, Eq, Ord)
-data Recipe = Recipe { ingredients :: [Resource], product :: Resource } deriving (Show, Eq, Ord)
+data Resource = Resource { quantity :: Int, name :: String } deriving (Show, Eq, Ord)
+data Recipe = Recipe { ingredients :: [Resource], produces :: Resource } deriving (Show, Eq, Ord)
 
-resource = liftM2 Resource (read <$> many1 digit) (space *> many1 upper) 
-recipe   = liftM2 Recipe (resource `sepBy` string ", ") (string " => " *> resource)
-recipes  = fromRight [] . parse (recipe `sepEndBy` newline) ""
+resource     = liftM2 Resource (read <$> many1 digit) (space *> many1 upper)
+recipe       = liftM2 Recipe (resource `sepBy` string ", ") (string " => " *> resource)
+parseRecipes = fromRight [] . parse (recipe `sepEndBy` newline) ""
 
-main = print . flip cost (Resource 1 "FUEL") . foldr save M.empty . recipes  =<< getContents 
-    where save (Recipe i (Resource _ name)) = M.insert name i
+main = print . ore . foldr save M.empty . parseRecipes  =<< getContents
+    where save r@(Recipe _ (Resource _ name)) = M.insert name r
 
-cost recipes (Resource x "ORE") = x
-cost recipes (Resource x name) = sum . map (cost recipes) . fromJust $  M.lookup name recipes
+ore recipes = flip (M.!) "ORE" . snd $ converge requirements (chems, M.singleton "FUEL" 1)
+    where requirements a = foldr (react recipes) a (M.keys recipes)
+          chems = M.fromList [ (item, 0) | item <- M.keys recipes ]
+
+react recipes item (chemicals,needs) =
+    let
+          has = M.findWithDefault 0 item chemicals
+          needed = M.findWithDefault 0 item needs
+          n = max 0 (needed - has)
+          (Recipe ingredients (Resource produced _)) = recipes M.! item
+          amount = (n + produced - 1) `div` produced
+          rest = amount * produced - needed
+          leftover = M.adjust (+ rest) item chemicals
+          requirements = M.fromList [ (name, amount * prod) | Resource prod name <- ingredients ]
+
+    in (leftover,M.unionWith (+) requirements (M.delete item needs))
+
+converge :: Eq a => (a -> a) -> a -> a
+converge = until =<< ((==) =<<)
