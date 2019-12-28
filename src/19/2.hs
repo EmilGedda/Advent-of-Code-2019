f8cc9d7ae1bb636c9c848732b8e97c7145034d73
Emil Gedda <emil.gedda@emilgedda.se>
2019-12-28 13:22:09 +0100

    day 19 part two

---
+++ src/19/2.hs
@@ -0,0 +1,20 @@
+import Data.Intcode
+import Data.Maybe
+import Control.Monad
+import Control.Arrow
+
+main = execStdin run
+
+run fx = head . mapMaybe (square fx) $ iterate (next fx) (3,4)
+
+pulls fx (x,y) = (==1) . fst . fromOutput $ fromInput (fromInput fx $ widen x) (widen y)
+
+next fx (x,y) =
+    let y' = y + 1
+        x' = fst . head . dropWhile (not . snd)
+           $ map (second (pulls fx)) [(n, (n, y')) | n <- [x..]]
+    in (x', y')
+
+square :: Effect -> (Int, Int) -> Maybe Int
+square fx (x,y) = guard (all (pulls fx) corners) >> return (x * 10000 + y - 99)
+    where corners = [(x,y), (x + 99, y), (x, y - 99), (x + 99, y - 99)]
