780b19d0ff09d0c9882b54ddd627136a0a329239
Emil Gedda <emil.gedda@emilgedda.se>
2019-12-29 18:58:10 +0100

    shorten

--- src/19/2.hs
+++ src/19/2.hs
@@ -2,19 +2,9 @@ import Data.Intcode
 import Data.Maybe
 import Data.List
 import Control.Monad
-import Control.Arrow
 
-main = execStdin run
+main = execStdin $ \fx -> head . mapMaybe (square fx) $ iterate (next fx) (3,4)
 
-run fx = head . mapMaybe (square fx) $ iterate (next fx) (3,4)
-
-pulls fx (x,y) = (==1) . fst . fromOutput $ fromInput (fromInput fx $ widen x) (widen y)
-
-next fx (x,y) =
-    let y' = y + 1
-        x' = fst . fromJust $ find (pulls fx) [(n, y') | n <- [x..]]
-    in (x', y')
-
-square :: Effect -> (Int, Int) -> Maybe Int
-square fx (x,y) = guard (all (pulls fx) corners) >> return (x * 10000 + y - 99)
-    where corners = [(x,y), (x + 99, y), (x, y - 99), (x + 99, y - 99)]
+pulls  fx coord = (==1) . head $ runList coord fx
+next   fx (x,y) = (head . fromJust $ find (pulls fx) [[n, y + 1] | n <- [x..]], y + 1)
+square fx (x,y) = guard (pulls fx [x,y] && pulls fx [x+99,y-99]) >> return (x * 10000 + y - 99)
