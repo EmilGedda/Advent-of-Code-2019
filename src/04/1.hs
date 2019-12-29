247505e16979eb6ddbaae5eb7c32cc27c769aece
Emil Gedda <emil.gedda@emilgedda.se>
2019-12-29 13:52:17 +0100

    cabalize day 4

--- src/04/1.hs
+++ src/04/1.hs
@@ -1,4 +1,3 @@
-#!/usr/bin/env runhaskell
 import Control.Monad
 import Data.List
 
@@ -6,7 +5,7 @@ main = print . length $ filter valid [236666..699999]
 
 valid = liftM2 (&&) sorted adjacent . digits
     where sorted xs = and $ zipWith (>=) xs (tail xs)
-          adjacent = or . map ((>=2) . length) . group
+          adjacent = any ((>=2) . length) . group
 
 digits 0 = []
 digits x = let (div, rem) = x `divMod` 10 in rem:digits div
